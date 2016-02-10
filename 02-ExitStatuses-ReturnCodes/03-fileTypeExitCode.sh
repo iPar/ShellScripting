8a566a46936f4d947dfa9497900570c60a169ae7
ipar <parha100@mail.chapman.edu>
2016-02-09 20:36:26 -0800

    Clarified comments in 02-ExitStatus... directory scripts 03 and 04

--- 02-ExitStatuses-ReturnCodes/03-fileTypeExitCode.sh
+++ 02-ExitStatuses-ReturnCodes/03-fileTypeExitCode.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# This script accepts a file or directory name as an argurment,
-# then reports if it is a regular file, a directory or other type
-# of file, exiting with codes of 0, 1 or 2, respectively.
+# This script accepts a file or directory name as an argurment, then
+# dependent on if it is a regular file, a directory or other type of
+# file, exits with codes of 0, 1 or 2, respectively.
 
 if [ -f ${1} ]
 then
--- 02-ExitStatuses-ReturnCodes/04-commandExitCode.sh
+++ 02-ExitStatuses-ReturnCodes/04-commandExitCode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script executes the command "cat /etc/shadow", returning a
-# 0 exit status code with a "Command succeeded" message, if
+# 0 exit status code with a "Command succeeded" message, if the cat
 # command itself returned a 0 exit status.  If the command returns
 # a non-zero exit status, a "Command failed" message is displayed
 # and a 1 exit status is reported.
