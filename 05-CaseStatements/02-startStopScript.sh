4dfe51277ba3f2ff376628bb8e59e22b7a926f48
ipar <parha100@mail.chapman.edu>
2016-02-11 00:42:09 -0800

    Renamed scripts in 05-CaseStatements/ and saved sleep-walking-server locally

--- 05-CaseStatements/02-startStopScript.sh
+++ 05-CaseStatements/02-startStopScript.sh
@@ -4,14 +4,14 @@
 # It accepts "start" and "stop" as arguments and provides an error message
 # and terminates with an exit status of 1 if anything else is received.
 # Be sure to copy the sleep-walking-server file into /tmp and
-# "chmod 755 /tmpsleep-walking-server"
+# "chmod 755 /tmp/sleep-walking-server"
 
 case "$1" in
   [sS][tT][aA][rR][tT])
     /tmp/sleep-walking-server &
     ;;
   [sS][tT][oO][pP])
-    kill $(cat /tmp/sleep-walking-server.pid)
+    kill -KILL $(cat /tmp/sleep-walking-server.pid)
     ;;
   *)
     echo "Usage: $0 start|stop"
