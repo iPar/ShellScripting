b15aea9c2a0fb67523842b43e8fb8fff371cee30
iPar <parha100@mail.chapman.edu>
2016-02-20 17:36:29 -0800

    Added first two solutions to LearnYouBash exercises.

---
+++ LearnYouBash/01-Hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+############################################################
+#
+# Simple 'hello world' program that outprints the subject
+# string to the screen via the 'echo' command (stdout).
+#
+############################################################
+
+echo "Hello, world!"
+
+exit 0
---
+++ LearnYouBash/02-Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+############################################################
+#
+# Simple script which demonstrates the use of envirnoment
+# variables in stdout.
+#
+############################################################
+
+echo "User ${USER} in directory ${PWD}."
